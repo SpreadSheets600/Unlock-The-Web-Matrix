bd602c64b4aa1c868c1f4400f7b7489fb4cd0050
SpreadSheets600 <sohammaity006@gmail.com>
2025-07-19 11:52:49 +0530

    Updated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreatePosts from "./pages/CreatePosts";
+import Layout from "./components/Layout";
+import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Posts from "./pages/Posts";
-import CreatePosts from "./pages/CreatePosts";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Posts />} />
-        <Route path="/create" element={<CreatePosts />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<Posts />} />
+					<Route path="/create" element={<CreatePosts />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	);
 }
 
 export default App;
